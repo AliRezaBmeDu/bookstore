dad9048412fac8c61b71a899aadda710ba3c52b4
AliReza <ali.reza.buet@gmail.com>
2023-08-29 22:18:43 +0600

    Fix the bugs for getBookItems

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Homepage from './pages/Homepage';
 import Categories from './pages/Categories';
 import Navigation from './components/Navigation';
-import { useDispatch, useSelector } from 'react-redux';
 import { getBookItems } from './redux/books/booksSlice';
 import './App.css';
 
 const App = () => {
-  const {books, isLoading} = useSelector((store) => store.books);
+  const { isLoading } = useSelector((store) => store.books);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
-    dispatch(getBookItems())
+    dispatch(getBookItems());
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <Router>
-    <div className="App">
-      <Navigation />
+      <div className="App">
+        <Navigation />
 
-      <Routes>
-        <Route path="/" exact element={<Homepage />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
-  </Router>
-  )
-}
+        <Routes>
+          <Route path="/" exact element={<Homepage />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,22 +1,34 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi'
-const appId = 'FfsxRXd6weDBOx32orzJ'
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const appId = 'FfsxRXd6weDBOx32orzJ';
 
 const initialState = {
   books: [],
   isLoading: true,
 };
 
+const makeProperList = (booksObject) => {
+  const result = [];
+  Object.keys(booksObject).forEach((itemId) => {
+    const book = booksObject[itemId][0];
+    book.item_id = itemId;
+    result.push(book);
+  });
+  return result;
+};
+
 export const getBookItems = createAsyncThunk('books/getBookItems', async () => {
-  try{
+  try {
     const response = await axios(`${baseUrl}/apps/${appId}/books`);
-    console.log(response.data)
-  }catch(err){
+    console.log(response.data);
+    return response.data;
+  } catch (err) {
     console.error('Error fetching data');
+    throw err;
   }
-})
+});
 
 const booksSlice = createSlice({
   name: 'books',
@@ -37,12 +49,13 @@ const booksSlice = createSlice({
     },
     [getBookItems.fulfilled]: (state, action) => {
       console.log('Data fetching complete');
+      state.books = makeProperList(action.payload);
       state.isLoading = false;
     },
     [getBookItems.rejected]: (state) => {
       state.isLoading = false;
-    }
-  }
+    },
+  },
 });
 
 export const { addBook, deleteBook } = booksSlice.actions;
