876c498ac2122c140f8d289339a1662a7d036bd9
AliReza <ali.reza.buet@gmail.com>
2023-08-29 21:03:24 +0600

    Call the getBookItems from the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Categories from './pages/Categories';
 import Navigation from './components/Navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBookItems } from './redux/books/booksSlice';
 import './App.css';
 
-const App = () => (
-  <Router>
+const App = () => {
+  const {books, isLoading} = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getBookItems())
+  }, []);
+
+  return (
+    <Router>
     <div className="App">
       <Navigation />
 
@@ -16,6 +26,7 @@ const App = () => (
       </Routes>
     </div>
   </Router>
-);
+  )
+}
 
 export default App;
