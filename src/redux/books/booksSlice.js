2ce1af80fc63821dbca447856bf47cfb86f8ac83
AliReza <ali.reza.buet@gmail.com>
2023-08-29 20:54:02 +0600

    Install axios. Create appId. Get the books

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,10 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
-import primaryBooks from '../../components/BookDB';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi'
+const appId = 'FfsxRXd6weDBOx32orzJ'
 
 const initialState = {
-  books: primaryBooks,
+  books: [],
   isLoading: true,
 };
+
+export const getBooks = createAsyncThunk('books/getBookItems', async () => {
+  try{
+    const response = await axios(`${baseUrl}/apps/${appId}/books`);
+    console.log(response.data)
+  }catch(err){
+    console.error('Error fetching data');
+  }
+})
 
 const booksSlice = createSlice({
   name: 'books',
