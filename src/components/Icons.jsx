9ff184bff53d49deb395658ea3358032d153123e
AliReza <ali.reza.buet@gmail.com>
2023-08-30 22:09:17 +0600

    Fix the linters

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -17,16 +17,13 @@ export const CartIcon = () => (
   </svg>
 );
 
-export const ChevronDown = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
-  >
-    <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+export const humanLoggedIn = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+    <circle cx="50" cy="50" r="45" fill="white" />
+
+    <circle cx="50" cy="30" r="15" fill="blue" />
+    <rect x="45" y="45" width="10" height="30" fill="blue" />
+    <circle cx="50" cy="70" r="10" fill="blue" />
   </svg>
 );
 
