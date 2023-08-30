a7395847e24255b44cdc5bcfc8e8b92e050b855c
AliReza <ali.reza.buet@gmail.com>
2023-08-30 12:41:45 +0600

    Fix the width problem

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CartIcon } from './Icons';
 
 function Navigation() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <h1 className="bookstore-cms">Bookstore CMS</h1>
-          </li>
-          <li>
-            <Link to="/" className="books-nav">BOOKS</Link>
-          </li>
-          <li>
-            <Link to="/categories" className="categories-nav">CATEGORIES</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <nav>
+      <ul>
+        <li>
+          <h1 className="bookstore-cms">Bookstore CMS</h1>
+        </li>
+        <li>
+          <Link to="/" className="books-nav">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="/categories" className="categories-nav">CATEGORIES</Link>
+        </li>
+      </ul>
+      <CartIcon />
+    </nav>
   );
 }
 
