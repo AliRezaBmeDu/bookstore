53de5bc22123cc99160674bcb53dc0ef7f25dfa9
AliReza <ali.reza.buet@gmail.com>
2023-08-30 16:26:40 +0600

    Add the circular progress bar

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteBook } from '../redux/books/booksSlice';
+import { CircularProgressBar } from './Icons';
 
 const BookItem = ({ book }) => {
   const dispatch = useDispatch();
   return (
-    <div className="single-book">
-      <div className="book-info">
-        <span className="book-category">{book.category}</span>
-        <span className="book-title">{book.title}</span>
-        <span className="book-author">{book.author}</span>
+    <div className="single-book-container">
+      <div className="single-book">
+        <div className="book-info">
+          <span className="book-category">{book.category}</span>
+          <span className="book-title">{book.title}</span>
+          <span className="book-author">{book.author}</span>
+        </div>
+        <div className="btn-container">
+          <button type="button">Comments</button>
+          <span>|</span>
+          <button type="button" onClick={() => dispatch(deleteBook(book))}>Remove</button>
+          <span>|</span>
+          <button type="button">Edit</button>
+        </div>
       </div>
-      <div className="btn-container">
-        <button type="button">Comments</button>
-        <button type="button" onClick={() => dispatch(deleteBook(book))}>Remove</button>
-        <button type="button">Edit</button>
+      <div className="mid-section">
+        <CircularProgressBar percentage={75} />
+        <div>
+          <span>75%</span>
+          <p>Completed</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const CartIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -27,3 +29,34 @@ export const ChevronDown = () => (
     <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
   </svg>
 );
+
+export const CircularProgressBar = ({ percentage }) => {
+  const radius = 40;
+  const circumference = 2 * Math.PI * radius;
+  const progress = circumference - (percentage / 100) * circumference;
+
+  return (
+    <div className="circular-progress-bar">
+      <svg className="circle">
+        <circle
+          className="circle-background"
+          r={radius}
+          cx={50}
+          cy={50}
+        />
+        <circle
+          className="circle-progress"
+          r={radius}
+          cx={50}
+          cy={50}
+          strokeDasharray={circumference}
+          strokeDashoffset={progress}
+        />
+      </svg>
+    </div>
+  );
+};
+
+CircularProgressBar.propTypes = {
+  percentage: PropTypes.number.isRequired,
+};
